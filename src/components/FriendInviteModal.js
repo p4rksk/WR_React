90e552897eb860d6a0496f97d0697e8bb7d158ee
p4rksk <your@email.com>
2025-07-19 00:50:22 +0900

    Feat: 사이드바 연결 모달 구현중

---
+++ src/components/FriendInviteModal.js
@@ -0,0 +1,79 @@
+import React, {useCallback ,useEffect, useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { Button } from 'react-bootstrap';
+import debounce from 'lodash/debounce';
+
+const dummyFriends = [
+    {id: 1, name:"김철수"},
+    {id: 2, name:"홍길동"}
+]
+
+function FriendInviteModal({show, handleClose}) {
+    const [search, setSearch] = useState('');
+    const [results, setResults] = useState([]);
+    const [selectedFriend, setSelectedFriend] = useState(null);
+
+    const debouncedSearch = useCallback(
+          debounce((value) => {
+            const filter = dummyFriends.filter(
+              (friend) => friend.name.includes(value) 
+            );
+            
+            setResults(filter);
+      }, 300),
+      [] // 의존성 없음 = 재생성 안되게 한다.          
+    );
+
+    useEffect = (() => {
+        if(search.trim() !== '') {
+            debouncedSearch(search);
+        }else{
+            setResults([]);
+            setSelectedFriend(null);
+        }
+    }, [search, debouncedSearch]);
+
+    const handleSelect = (friend) => {
+    setSelectedFriend(friend);
+    setSearch(friend.name); 
+    setResults([]); 
+  };
+    
+    const handleInvite = () => {
+        if (!selectedFriend) {
+            alert('친구를 선택해주세요')
+        return
+        }
+
+        
+    }
+
+
+
+    return (
+        <div>
+            <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>친구추가</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="d-flex flex-column gap-2">
+          <label htmlFor="friendNameInput">친구 이름</label>
+          <input type="text" id="friendNameInput" value={search} onChange={(e)=> setSearch(e.target.value)} placeholder="이름을 입력하세요"/>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary">친구추가</Button>
+        </Modal.Footer>
+      </Modal>
+        </div>
+    );
+}
+
+export default FriendInviteModal;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,27 @@
-import { Button, Nav } from 'react-bootstrap';
+import { Button, Nav, OverlayTrigger, Popover } from 'react-bootstrap';
 import '../css/Sidebar.css';
 import SidebarProfile from './SidebarProfile';
-import { OverlayTrigger, Popover } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import FriendInviteModal from './FriendInviteModal';
+
+
 
 function Sidebar() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+
+
   const teamPopover = (
   <Popover id="team-popover" className="bg-dark text-white border-0">
     <Popover.Body>
-      <div className="dropdown-item text-white">A팀</div>
-      <div className="dropdown-item text-white">팀 가입</div>
+      <Link href='' className="dropdown-item text-white">A팀</Link>
+      <hr className="border-light" />
+      <Button className="dropdown-item text-white">팀 가입</Button>
+      <Button className="dropdown-item text-white">팀 생성</Button>
+      <Button className="dropdown-item text-white">팀 초대</Button>
     </Popover.Body>
   </Popover>
 );
@@ -17,7 +30,7 @@ function Sidebar() {
     <>
       <div className="sidebar">
         <h4 className="mt-2 mb-2">WR</h4>
-        <hr className="border-secondary" />
+        <hr className="border-secondary"/>
         <Nav className="mt-4 flex-column gap-3">
           <Nav.Link href="#home">홈</Nav.Link>    
           <Nav.Link href="#notifications">알림함</Nav.Link>
@@ -37,18 +50,14 @@ function Sidebar() {
         </Nav>
         <div className="mt-auto p-3 bg-dark text-white rounded">
           친구를 초대하고 <br /> 팀을 만들어보세요!
-          <Button
-            variant="light"
-            size="sm"
-            className="mt-2 w-100"
-            onClick={() => alert('모달 열기 구현 예정')}
-          >
+          <Button variant="light" size="sm" className="mt-2 w-100" onClick={handleShow}>
             + 친구 추가
           </Button>
         </div>
-        <hr></hr>
+        <hr className="border-light" />
         <SidebarProfile/>
       </div>
+      <FriendInviteModal show={true} handleClose={handleClose}/>
     </>
   );
 }
